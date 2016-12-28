4f9884444d71ed8ab63729490e0c75b0d0e13848
Christopher Motl <cmotl@cmotl.com>
2016-12-28 00:03:35 -0600

    [Day 7 - part 2]

--- day_7/src/ipv7.erl
+++ day_7/src/ipv7.erl
@@ -1,12 +1,12 @@
 -module(ipv7).
--export([parse/1, abba/1, supports_tls/1]).
+-export([parse/1, abba/1, supports_tls/1, aba/1, extract_abas/1, invert_aba/1, supports_ssl/1]).
 
 parse(Ip) -> 
     Components = re:split(Ip,"(\\[|\\])",[{return,list}, group]),
-{UnTrimmedHypernetSequences, UnTrimmedNonHypernetSequences} = lists:partition(fun(X) -> lists:member("]", X) end, Components),
+{UnTrimmedHypernetSequences, UnTrimmedSupernetSequences} = lists:partition(fun(X) -> lists:member("]", X) end, Components),
     HypernetSequences = re:split(lists:flatten(UnTrimmedHypernetSequences), "]",[{return,list}, trim]),
-    NonHypernetSequences = re:split(lists:flatten(UnTrimmedNonHypernetSequences), "[[]", [{return, list}, trim]),
-    {NonHypernetSequences, HypernetSequences}.
+    SupernetSequences = re:split(lists:flatten(UnTrimmedSupernetSequences), "[[]", [{return, list}, trim]),
+    {SupernetSequences, HypernetSequences}.
 
 abba([]) -> false;
 abba([_|[]]) -> false;
@@ -16,7 +16,33 @@ abba([A,B,B,A|_]) when A =/= B -> true;
 abba([_,_,_,_|[]]) -> false;
 abba([_,A,B,C|Rest]) -> abba([A,B,C|Rest]).
 
-supports_tls({NHNS, HNS}) -> 
-    TlsInNhns = lists:member(true, lists:map(fun(X) -> abba(X) end, NHNS)),
+aba([]) -> false;
+aba([_|[]]) -> false;
+aba([_,_|[]]) -> false;
+aba([A,B,A|[]]) when A =/= B -> true;
+aba([_,_,_|[]]) -> false.
+
+
+extract_abas(Input) -> extract_abas(Input, []).
+
+extract_abas([_,_|[]], Abas) -> Abas;
+extract_abas([A,B,C|Rest], Abas) ->
+    case aba([A,B,C|[]]) of
+        true -> extract_abas([B,C|Rest], Abas ++ [[A,B,C]]);
+        _ -> extract_abas([B,C|Rest],Abas)
+    end.
+
+invert_aba([A,B,A]) -> [B,A,B].
+
+
+supports_tls({SNS, HNS}) -> 
+    TlsInSns = lists:member(true, lists:map(fun(X) -> abba(X) end, SNS)),
     TlsInHns = lists:member(true, lists:map(fun(X) -> abba(X) end, HNS)),
-    TlsInNhns and not TlsInHns.
+    TlsInSns and not TlsInHns.
+
+supports_ssl({SNS, HNS}) -> 
+    Abas = lists:foldl(fun(X, Xs) -> ipv7:extract_abas(X) ++ Xs end, [], SNS),
+    Babs = lists:map(fun ipv7:invert_aba/1, lists:foldl(fun(X, Xs) -> ipv7:extract_abas(X) ++ Xs end , [], HNS)),
+    Matches = sets:intersection(sets:from_list(Abas), sets:from_list(Babs)),
+    sets:size(Matches) >= 1.
+
--- day_7/test/ipv7_test.erl
+++ day_7/test/ipv7_test.erl
@@ -1,7 +1,7 @@
 -module(ipv7_test).
 -include_lib("eunit/include/eunit.hrl").
 
--import(ipv7, [parse/1, abba/1, supports_tls/1]).
+-import(ipv7, [parse/1, abba/1, supports_tls/1, aba/1, extract_abas/1, invert_aba/1, supports_ssl/1]).
 
 should_parse_line_with_one_hypernet_sequence_test() -> 
     Ip = "a[b]",
@@ -9,23 +9,55 @@ should_parse_line_with_one_hypernet_sequence_test() ->
     Actual = parse(Ip),
     ?assertEqual(Expected, Actual).
 
-should_parse_line_with_one_hypernet_sequence_and_two_non_test() -> 
+should_parse_line_with_one_hypernet_sequence_and_two_supernet_sequences_test() -> 
     Ip = "a[b]c",
     Expected = {["a", "c"], ["b"]},
     Actual = parse(Ip),
     ?assertEqual(Expected, Actual).
 
-should_parse_line_with_two_hypernet_sequences_and_two_non_test() -> 
+should_parse_line_with_two_hypernet_sequences_and_two_supernet_sequences_test() -> 
     Ip = "a[b]c[d]",
     Expected = {["a", "c"], ["b", "d"]},
     Actual = parse(Ip),
     ?assertEqual(Expected, Actual).
 
-should_parse_line_with_two_hypernet_sequences_and_three_non_test() -> 
+should_parse_line_with_two_hypernet_sequences_and_three_supernet_sequences_test() -> 
     Ip = "a[b]c[d]e",
     Expected = {["a", "c", "e"], ["b", "d"]},
     Actual = parse(Ip),
     ?assertEqual(Expected, Actual).
+
+
+should_invert_aba_test() ->
+    ?assertEqual("bab", invert_aba("aba")).
+
+
+should_not_find_aba_in_string_less_than_3_characters_in_length_test() ->
+    ?assertEqual(false, aba("")),
+    ?assertEqual(false, aba("a")),
+    ?assertEqual(false, aba("aa")).
+
+should_not_find_aba_when_inner_characters_are_not_different_test() ->
+    ?assertEqual(false, aba("aaa")).
+
+should_find_aba_aba_string_test() ->
+    ?assertEqual(true, aba("aba")).
+
+
+should_find_one_aba_string_in_aba_string_test() ->
+    ?assertEqual(["aba"], extract_abas("aba")).
+
+should_find_no_aba_string_in_non_aba_string_test() ->
+    ?assertEqual([], extract_abas("aaa")).
+
+should_find_one_aba_string_in_aba_string_with_trailing_characters_test() ->
+    ?assertEqual(["aba"], extract_abas("abaccc")).
+
+should_find_one_aba_string_in_aba_string_with_leading_characters_test() ->
+    ?assertEqual(["aba"], extract_abas("cccaba")).
+
+should_find_two_aba_strings_test() ->
+    ?assertEqual(["xyx", "aba"], extract_abas("xyxaba")).
 
 
 should_not_find_abba_in_string_less_than_4_characters_in_length_test() ->
@@ -54,23 +86,34 @@ should_not_find_abba_in_nonabba_string_with_more_than_4_characters_test() ->
 
 
 
-should_support_tls_when_abba_in_non_hyper_net_sequences_test() ->
+should_support_tls_when_abba_in_supernet_sequences_test() ->
     HNS = ["mnop"],
-    NHNS = ["abba", "qrst"],
-    ?assertEqual(true, supports_tls({NHNS, HNS})).
+    SNS = ["abba", "qrst"],
+    ?assertEqual(true, supports_tls({SNS, HNS})).
 
 should_not_support_tls_when_abba_in_hyper_net_sequences_test() ->
     HNS = ["bddb"],
-    NHNS = ["abcd", "xyyx"],
-    ?assertEqual(false, supports_tls({NHNS, HNS})).
+    SNS = ["abcd", "xyyx"],
+    ?assertEqual(false, supports_tls({SNS, HNS})).
 
-should_not_support_tls_when_abba_not_in_non_hyper_net_sequences_test() ->
+should_not_support_tls_when_abba_not_in_supernet_sequences_test() ->
     HNS = ["qwer"],
-    NHNS = ["aaaa", "tyui"],
-    ?assertEqual(false, supports_tls({NHNS, HNS})).
+    SNS = ["aaaa", "tyui"],
+    ?assertEqual(false, supports_tls({SNS, HNS})).
 
-should_support_tls_when_abba_in_larger_string_in_non_hyper_net_sequences_test() ->
+should_support_tls_when_abba_in_larger_string_in_supernet_sequences_test() ->
     HNS = ["asdfgh"],
-    NHNS = ["ioxxoj", "zxcvbn"],
-    ?assertEqual(true, supports_tls({NHNS, HNS})).
+    SNS = ["ioxxoj", "zxcvbn"],
+    ?assertEqual(true, supports_tls({SNS, HNS})).
+
+should_support_ssl_when_aba_in_supernet_has_coresponding_bab_in_hypernet_test() ->
+    HNS = ["bab"],
+    SNS = ["aba","xyz"],
+    ?assertEqual(true, supports_ssl({SNS, HNS})).
+
+should_support_ssl_when_two_abas_overlapn_in_supernet_and_has_corresponding_bab_in_hypernet_test() ->
+    HNS = ["bzb"],
+    SNS = ["zazbz","cdb"],
+    ?assertEqual(true, supports_ssl({SNS, HNS})).
+
 
