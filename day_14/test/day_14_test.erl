46ec7ff0b5e2ae3e14b43bbdf475f124d4301ee6
Christopher Motl <cmotl@cmotl.com>
2017-01-28 07:57:55 -0600

    [Day 14] Execution

--- day_14/test/day_14_test.erl
+++ day_14/test/day_14_test.erl
@@ -8,3 +8,13 @@ part1_integration_test() ->
 part2_integration_test() -> 
     Salt = "cuanljph",
     ?assertEqual(8, length(Salt)).
+
+
+
+% part 1
+% one_time_pad:generate_keys("cuanljph", 0, 64).
+
+
+% part 2
+% hash_server:start("cuanljph").
+% secure_one_time_pad:generate_keys("", 0, 64). 
